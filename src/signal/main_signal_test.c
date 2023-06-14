56d25236dba1715ee4c8d46295a76beb6c88d813
Gauthier Bertet <gbertet@2D6.42angouleme.fr>
2023-06-14 20:06:37 +0200

    Unlink the .heredoc file after each exec

--- src/signal/main_signal_test.c
+++ src/signal/main_signal_test.c
@@ -6,7 +6,7 @@
 /*   By: gbertet <gbertet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 12:58:15 by lamasson          #+#    #+#             */
-/*   Updated: 2023/06/14 17:35:12 by gbertet          ###   ########.fr       */
+/*   Updated: 2023/06/14 20:05:57 by gbertet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,11 @@ int	main(int argc, char **argv, char **env)
 			if (synthax_check(mish.full_cmd))
 			{
 				get_cmds(&mish);
-				ft_heredoc(mish.cmds->c);
 				ft_call_pipex(&mish);
 				ft_free_cmds(&mish);
 			}
 		}
+		unlink(".heredoc");
 	}
 	free(tmp);
 	free(prompt);
